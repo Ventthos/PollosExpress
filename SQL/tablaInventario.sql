8b08ce37c85cf4993ca6a2bff3182b83a1c9c84b
GanzytoX <gonzalo.gb11.ggb@gmail.com>
2024-02-26 07:29:05 -0600

    Modificaciones en inventario

--- SQL/tablaInventario.sql
+++ SQL/tablaInventario.sql
@@ -7,19 +7,20 @@ SELECT * FROM producto;
 
 CREATE TABLE inventario (
     id_producto INT NOT NULL,
-    nombre_producto VARCHAR(100) NOT NULL,
+    nombre_producto NVARCHAR(100) NOT NULL,
     unidad VARCHAR(3) NOT NULL,
     cantidad INT NOT NULL,
     estado BOOLEAN NOT NULL,
     PRIMARY KEY(id_producto),
     FOREIGN KEY(id_producto) REFERENCES producto(id_producto)
-    #FOREIGN KEY(estado) REFERENCES producto(activo)
 );
 
 #INSERT INTO inventario (id_producto, nombre_producto, unidad, cantidad, estado) VALUES (20, 'Mollo con pole', 'KG', 10, 0);
-INSERT INTO inventario (id_producto, nombre_producto, unidad, cantidad, estado) VALUES (31, 'Pollo asado', 'KG', 20, TRUE);
-INSERT INTO inventario (id_producto, nombre_producto, unidad, cantidad, estado) VALUES (34, 'Hace algo', '--', 20, TRUE);
-INSERT INTO inventario (id_producto, nombre_producto, unidad, cantidad, estado) VALUES (35, 'Nuevo producto', '--', 0, TRUE);
-INSERT INTO inventario (id_producto, nombre_producto, unidad, cantidad, estado) VALUES (36, 'Prueba', '--', 0, TRUE);
+#INSERT INTO inventario (id_producto, nombre_producto, unidad, cantidad, estado) VALUES (31, 'Pollo asado', 'KG', 20, TRUE);
+#INSERT INTO inventario (id_producto, nombre_producto, unidad, cantidad, estado) VALUES (34, 'Hace algo', '--', 20, TRUE);
+#INSERT INTO inventario (id_producto, nombre_producto, unidad, cantidad, estado) VALUES (35, 'Nuevo producto', '--', 0, TRUE);
+#INSERT INTO inventario (id_producto, nombre_producto, unidad, cantidad, estado) VALUES (36, 'Prueba', '--', 0, TRUE);
+INSERT INTO inventario (id_producto, nombre_producto, unidad, cantidad, estado) VALUES (47, 'Yopo asado', '--', 0, FALSE);
+INSERT INTO inventario (id_producto, nombre_producto, unidad, cantidad, estado) VALUES (50, 'Poyo', '--', 0, TRUE);
 
 SELECT * FROM inventario;
