143ccb3d16aba7f1ad7b1e17b9ab82a78ef0a3ea
Ventthos <117321816+Ventthos@users.noreply.github.com>
2024-03-25 18:32:12 -0600

    Ventas casi completado

--- Interfaces/Analisis.py
+++ Interfaces/Analisis.py
@@ -31,19 +31,40 @@ class Analisis(QMainWindow, Ui_MainWindow):
         self.figura = plt.Figure()
         self.canvas = FigureCanvas(self.figura)
 
-        self.horizontalLayout_4.addWidget(self.canvas)
-        self.buttonBuscar.clicked.connect(self.graficquita)
+        self.horizontalLayout_5.addWidget(self.canvas)
 
-    def graficquita(self):
-        
+        # datosGrafica
+        self.ventasGraphic = []
+        self.gastosGrapic = []
+
+        # Conectar eventos de cambiar graficas
+        self.pushButtonGraf_L.clicked.connect(self.changeGraphicToVentas)
+        self.pushButtonGraf_R.clicked.connect(self.changeGraphicToGastos)
+
+        # Inicializar el programa
+        self.setFecha()
+
+    def changeGraphicToGastos(self):
+        if self.labelPaginaDatos_Graf.text()[0] == "1":
+            self.setGrafic(self.gastosGrapic[0], self.gastosGrapic[1],self.gastosGrapic[2],
+                           self.gastosGrapic[3],self.gastosGrapic[4])
+        self.labelPaginaDatos_Graf.setText("2/2")
+
+    def changeGraphicToVentas(self):
+        if self.labelPaginaDatos_Graf.text()[0] == "2":
+            self.setGrafic(self.ventasGraphic[0], self.ventasGraphic[1], self.ventasGraphic[2],
+                           self.ventasGraphic[3], self.ventasGraphic[4])
+        self.labelPaginaDatos_Graf.setText("1/2")
+
+    def setGrafic(self, datos, names, title, xLabel, yLabel):
         self.figura.clear()
-        cosas = ["aa", "ee", "ii"]
-        valores = [3, 2, 4]
-        ax = self.figura.add_subplot(111)  # Añadir un subplot a la figura
+        cosas = names
+        valores = datos
+        ax = self.figura.add_subplot(111)  # Añadir una barra
         ax.bar(cosas, valores, color='red', width=0.4)
-        ax.set_title("Capaz")  # Establecer el título del gráfico
-        ax.set_xlabel("aaa")  # Establecer la etiqueta del eje x
-        ax.set_ylabel("siiii")  # Establecer la etiqueta del eje y
+        ax.set_title(title)  # Establecer el título del gráfico
+        ax.set_xlabel(xLabel)  # Establecer la etiqueta del eje x
+        ax.set_ylabel(yLabel)  # Establecer la etiqueta del eje y
         self.canvas.draw()
 
     def calcularVentas(self, fecha):
@@ -64,28 +85,56 @@ class Analisis(QMainWindow, Ui_MainWindow):
 
     def calcularSumasDia(self, fecha):
         self.conection.commit()
-        script = (f"SELECT SUM(total_De_Compra) FROM venta WHERE WEEK(fecha_De_Venta,1) = week(\"{fecha}\",1) "
+        script = (f"SELECT DATE(fecha_De_Venta), SUM(total_De_Compra) FROM venta WHERE WEEK(fecha_De_Venta,1) = week(\"{fecha}\",1) "
                   f"AND YEAR(fecha_De_Venta) = YEAR(\"{fecha}\") GROUP BY DATE(fecha_De_Venta);")
         self.cursor.execute(script)
         sumas = self.cursor.fetchall()
 
+        fecha -= datetime.timedelta(days=1)
         sumasSimplidified = []
-        for arreglo in sumas:
-            sumasSimplidified.append(arreglo[0])
+        fechas = []
+        for i in range(7):
+            fecha = fecha + datetime.timedelta(days=1)
+            fechas.append(fecha)
 
+        for fechaInd in fechas:
+            encontrado = False
+            for elemento in sumas:
+                if elemento[0] == fechaInd:
+                    encontrado = True
+                    sumasSimplidified.append(elemento[1])
+            if not encontrado:
+                sumasSimplidified.append(0)
+
+        print("sumas finales")
+        print(sumasSimplidified)
         return sumasSimplidified
 
-    def calcularGastosDia(self, fecha):
+    def calcularGastosDia(self, fecha:datetime.date):
         self.conection.commit()
-        script = (f"SELECT SUM(monto) FROM gasto WHERE WEEK(fecha,1) = week(\"{fecha}\",1) "
+        script = (f"SELECT DATE(fecha), SUM(monto) FROM gasto WHERE WEEK(fecha,1) = week(\"{fecha}\",1) "
                   f"AND YEAR(fecha) = YEAR(\"{fecha}\") GROUP BY DATE(fecha);")
         self.cursor.execute(script)
         sumas = self.cursor.fetchall()
 
+        fecha -= datetime.timedelta(days=1)
         sumasSimplidified = []
-        for arreglo in sumas:
-            sumasSimplidified.append(arreglo[0])
+        fechas = []
+        for i in range(7):
+            fecha = fecha + datetime.timedelta(days=1)
+            fechas.append(fecha)
 
+        for fechaInd in fechas:
+            encontrado = False
+            for elemento in sumas:
+                if elemento[0] == fechaInd:
+                    encontrado = True
+                    sumasSimplidified.append(elemento[1])
+            if not encontrado:
+                sumasSimplidified.append(0)
+
+        print("sumas finales")
+        print(sumasSimplidified)
         return sumasSimplidified
 
     def rewind(self):
@@ -125,6 +174,13 @@ class Analisis(QMainWindow, Ui_MainWindow):
         sumaTotal = round(sumaTotal / 7, 2)
         self.lineEditPromedioGananciaDia.setText(f"${sumaTotal}")
 
+        self.ventasGraphic = [sumas, ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"],
+                              "Ventas", "Dia", "Total"]
+
+        self.setGrafic(sumas, ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"],
+                       "Ventas", "Dia", "Total")
+        self.labelPaginaDatos_Graf.setText("1/2")
+
         # Calcula gastos
         sumas = self.calcularGastosDia(monday)
         sumaTotal = 0
@@ -133,11 +189,16 @@ class Analisis(QMainWindow, Ui_MainWindow):
 
         sumaTotal = round(sumaTotal / 7, 2)
         self.PromGastos.setText(f"${sumaTotal}")
+
+        self.gastosGrapic = [sumas, ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"],
+                              "Gastos", "Dia", "Total"]
+
+
+
         #############
         monday = monday.strftime("%d/%m/%Y")
         sunday = sunday.strftime("%d/%m/%Y")
         self.labelTiempo.setText(f"Semana {monday} - {sunday}")
-
 
     def calcularSemana(self):
         today = datetime.date.today()
